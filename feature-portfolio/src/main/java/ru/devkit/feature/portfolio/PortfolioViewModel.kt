944a4d8a7e5ffff4179305c9bf78a1b0773d4087
k.i.tayupov <k.i.tayupov@tinkoff.ru>
2024-01-02 20:28:23 +0300

    prepare portfolio view model

--- feature-portfolio/src/main/java/ru/devkit/feature/portfolio/PortfolioViewModel.kt
+++ feature-portfolio/src/main/java/ru/devkit/feature/portfolio/PortfolioViewModel.kt
@@ -1,7 +1,28 @@
 package ru.devkit.feature.portfolio
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import ru.devkit.domain.repository.PortfolioRepository
+import ru.devkit.feature.portfolio.mapper.PortfolioDomainToUiMapper
+import ru.devkit.ui.model.PortfolioUiModel
+import javax.inject.Inject
 
-internal class PortfolioViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class PortfolioViewModel @Inject constructor(
+    private val repository: PortfolioRepository,
+    private val mapper: PortfolioDomainToUiMapper,
+) : ViewModel() {
+
+    private val _model = MutableStateFlow(PortfolioUiModel.EMPTY)
+    val model = _model.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            repository.data.collect {
+                _model.value = mapper(it)
+            }
+        }
+    }
 }
---
+++ feature-portfolio/src/main/java/ru/devkit/feature/portfolio/mapper/PortfolioDomainToUiMapper.kt
@@ -0,0 +1,37 @@
+package ru.devkit.feature.portfolio.mapper
+
+import ru.devkit.domain.repository.data.Portfolio
+import ru.devkit.ui.model.DiffSign
+import ru.devkit.ui.model.LastSign
+import ru.devkit.ui.model.ListItemUiModel
+import ru.devkit.ui.model.PortfolioUiModel
+import ru.devkit.utils.formatCurrencyPrice
+import ru.devkit.utils.formatPercentage
+import javax.inject.Inject
+
+/**
+ * @author k.i.tayupov
+ */
+class PortfolioDomainToUiMapper @Inject constructor() {
+
+    operator fun invoke(from: Portfolio): PortfolioUiModel {
+        val items = from.investments.map {
+            ListItemUiModel(
+                id = it.id,
+                name = it.name,
+                value = formatCurrencyPrice(it.value),
+                diffValue = formatCurrencyPrice(it.difference),
+                diffPercentage = formatPercentage(it.difference / it.initialValue * 100),
+                diffSign = DiffSign.get(it.difference),
+                lastSign = LastSign.get(it.lastDifference),
+            )
+        }
+        return PortfolioUiModel(
+            items = items,
+            totalValue = formatCurrencyPrice(from.totalValue),
+            totalDiffValue = formatCurrencyPrice(from.totalDifference),
+            totalDiffPercentage = formatPercentage(from.totalDifference / from.totalInitialValue * 100),
+            totalDiffSign = DiffSign.get(from.totalDifference),
+        )
+    }
+}
