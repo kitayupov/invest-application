b6d5795e3a646c3bd8f7a55ffffc9b28aaa9bb3d
k.i.tayupov <k.i.tayupov@tinkoff.ru>
2024-01-02 14:14:24 +0300

    create app instance

---
+++ app/src/main/java/ru/devkit/investapplication/app/App.kt
@@ -0,0 +1,13 @@
+package ru.devkit.investapplication.app
+
+import android.app.Application
+
+/**
+ * @author k.i.tayupov
+ */
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
