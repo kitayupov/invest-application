7243b14a05be8aac51511bd29d661875c8197cb5
k.i.tayupov <k.i.tayupov@tinkoff.ru>
2024-01-05 01:49:52 +0300

    fix crash

--- service-impl/src/main/java/ru/devkit/service/data/SandboxPortfolioService.kt
+++ service-impl/src/main/java/ru/devkit/service/data/SandboxPortfolioService.kt
@@ -1,8 +1,13 @@
 package ru.devkit.service.data
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.cancellable
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import ru.devkit.service.PortfolioServiceApi
 import ru.devkit.service.api.portfolio.SandboxPortfolioService
 import ru.devkit.service.api.portfolio.data.request.AccountRequest
@@ -14,18 +19,32 @@ import javax.inject.Singleton
  * @author k.i.tayupov
  */
 @Singleton
-class SandboxPortfolioService @Inject constructor() : PortfolioServiceApi {
+class SandboxPortfolioService @Inject constructor() : PortfolioServiceApi, CoroutineScope by MainScope() {
 
     private val api = NetworkService.create(SandboxPortfolioService::class.java)
 
-    private var isStarted = true
+    private var portfolio = PortfolioApi(emptyList())
+
+    private var job: Job = Job()
 
     override fun getPortfolio(): Flow<PortfolioApi> {
-        return flow {
+        job.cancel()
+        val flow = flow {
+            while (true) {
+                emit(portfolio)
+                delay(1_000)
+            }
+        }
+        job = launch { flow.cancellable() }
+        return flow
+    }
+
+    override fun attach() {
+        launch {
             val accountId = api.getAccounts().accounts[0].id
             val orders = api.getSandboxOrders(AccountRequest(accountId))
 
-            val investments = orders.orders.map { order ->
+            val investments = orders.orders.distinctBy { it.figi }.map { order ->
                 InvestmentApi(
                     symbol = order.figi,
                     quantity = order.lotsRequested.toInt(),
@@ -33,18 +52,11 @@ class SandboxPortfolioService @Inject constructor() : PortfolioServiceApi {
                 )
             }
 
-            while (isStarted) {
-                emit(PortfolioApi(investments))
-                delay(1_000)
-            }
+            portfolio = PortfolioApi(investments)
         }
     }
 
-    override fun attach() {
-        isStarted = true
-    }
-
     override fun release() {
-        isStarted = false
+        job.cancel()
     }
 }
